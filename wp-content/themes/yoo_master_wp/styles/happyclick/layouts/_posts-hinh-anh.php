78de9948f534f95d460b819775bc821da72be071
Nguyen Tam <nguyentam.gm@gmail.com>
2013-07-19 10:52:19 +0700

    template config, wowslider, category hinh anh

---
+++ wp-content/themes/yoo_master_wp/styles/happyclick/layouts/_posts-hinh-anh.php
@@ -0,0 +1,31 @@
+<?php 
+/**
+* @package   Warp Theme Framework
+* @author    YOOtheme http://www.yootheme.com
+* @copyright Copyright (C) YOOtheme GmbH
+* @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
+*/
+
+// init vars
+//$colcount = is_front_page() ? $this['config']->get('multicolumns', 1) : 1;
+//$count    = $this['system']->getPostCount();
+//$rows     = ceil($count / $colcount);
+//$columns  = array();
+//$row      = 0;
+//$column   = 0;
+//$i        = 0;
+
+//Khoa hoc sap dien ra
+$args = array(
+    'category_name' => 'hinh-anh',
+    'post_status' => array( 'publish' ),
+    'posts_per_page' => 21,
+    'orderby' => 'date',
+    'order' => 'ASC'
+);
+
+$the_query = new WP_Query( $args );
+while ( $the_query->have_posts() ) {
+	$the_query->the_post();
+    echo $this->render('_post-hinh-anh');
+}
