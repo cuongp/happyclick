dfc48edc00071269a46d7a77bf511238dd2532a7
buihaian <haian@tgm.vn>
2013-07-21 23:15:04 +0700

    Improve flow dang ky

--- wp-content/themes/yoo_master_wp/styles/happyclick/layouts/xac-nhan-email.php
+++ wp-content/themes/yoo_master_wp/styles/happyclick/layouts/xac-nhan-email.php
@@ -1,10 +1,11 @@
-<div class="box" style="width:400px;">
+<div class="box" style="width:500px;">
 	<p>Happy Click đã gửi email cho bạn, vui lòng kiểm tra email và làm theo hướng dẫn trước khi bắt đầu dùng thử dịch vụ của Happy Click.</p>
 
-<h3>Lưu ý:</h3>
+<h3 style="color: #e58a35;">Lưu ý:</h3>
 <ul>
-<li>
-Nếu không tìm thấy email của Happy Click trong hộp thư đến, bạn có thể kiểm tra lại trong hộp thư rác.</li>
-<li>Nếu vẫn không tìm thấy email, vui lòng thực hiện lại.</li></ul>
+<li style="padding-left: 10px;">- Nếu không tìm thấy email của Happy Click trong hộp thư đến, bạn có thể kiểm tra lại trong hộp thư rác.</li>
+<li style="padding-left: 10px;">- Nếu vẫn không tìm thấy email, vui lòng thực hiện lại.</li>
+</ul>
+<span style="text-align:right"> <a href="htt://dev.happyclick.vn/">Trở về trang chủ </a></span>
 
 </div>
---
+++ wp-content/themes/yoo_master_wp/styles/happyclick/layouts/xac-nhan-thanh-cong.php
@@ -0,0 +1,10 @@
+<div class="box" style="width:500px;">
+  <p>Email của bạn đã được xác thực. Hãy đăng nhập để bắt đầu sử dụng dịch vụ của Happy</p>
+
+<ul>
+<li style="padding-left: 10px;">Email.</li>
+<li style="padding-left: 10px">Password</li>
+</ul>
+<span style="text-aligh:right"> <a href="htt://dev.happyclick.vn/">Trở về trang chủ </a></span>
+
+</div>
