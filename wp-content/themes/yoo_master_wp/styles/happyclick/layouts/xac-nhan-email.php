178b64d6ea918ff85f82c4e7b9be49e30d2487dc
buihaian <haian@tgm.vn>
2013-07-21 22:24:37 +0700

    Update widget name

--- wp-content/themes/yoo_master_wp/styles/happyclick/layouts/xac-nhan-email.php
+++ wp-content/themes/yoo_master_wp/styles/happyclick/layouts/xac-nhan-email.php
@@ -1,4 +1,4 @@
-<div class="box">
+<div class="box" style="width:400px;">
 	<p>Happy Click đã gửi email cho bạn, vui lòng kiểm tra email và làm theo hướng dẫn trước khi bắt đầu dùng thử dịch vụ của Happy Click.</p>
 
 <h3>Lưu ý:</h3>
