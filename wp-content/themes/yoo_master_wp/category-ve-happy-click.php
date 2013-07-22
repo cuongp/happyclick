3e6c29aa656e9718b93470a011bf86e1b2c1a0bf
vu nguyen <anhvunguyen2211@gmail.com>
2013-07-22 16:57:02 +0700

    update code

---
+++ wp-content/themes/yoo_master_wp/category-ve-happy-click.php
@@ -0,0 +1,3 @@
+<?php
+
+wp_redirect(get_site_url().'/ve-happy-click');
--- wp-content/themes/yoo_master_wp/layouts/template.config.php
+++ wp-content/themes/yoo_master_wp/layouts/template.config.php
@@ -178,3 +178,11 @@ if ($this['useragent']->browser() == 'msie') {
 if (isset($head)) {
 	$this['template']->set('head', implode("\n", $head));
 }
+
+function string_limit_words($string, $word_limit)
+{
+  $words = explode(' ', $string, ($word_limit + 1));
+  if(count($words) > $word_limit)
+  array_pop($words);
+  return implode(' ', $words);
+}
