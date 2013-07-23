8f4c0543a0724d1bb9ba8205452bdc53a642c6a5
Pham Minh Cuong <minhcuong@siliconstraits.vn>
2013-07-23 14:24:14 +0700

    Update Dang xuat

--- wp-content/themes/yoo_master_wp/styles/happyclick/layouts/dang-xuat.php
+++ wp-content/themes/yoo_master_wp/styles/happyclick/layouts/dang-xuat.php
@@ -1,10 +1,9 @@
-<div class="box" style="width:500px;">
-  
-<p>Bạn đã đăng xuất khỏi trang happyclick.com.vn</p>
-<p>Happy Click hy vọng bạn đã có những trải nghiệm thú vị và bổ ích.</p>
-<p>Hãy sớm quay lại để cùng Happy Click thăng tiến mỗi ngày nhé.</p>
-<p>Cảm ơn bạn!</p>
+<div class="box" style="width:760px; text-align:center;margin-top:5px">
+ <p><img src="http://dev.happyclick.vn/wp-content/uploads/2013/07/model.jpg" /></p>
+<p style="line-height:18px">Bạn đã đăng xuất khỏi trang <a href="http://happyclick.com.vn">happyclick.com.vn</a></p>
+<p style="line-height:18px">Happy Click hy vọng bạn đã có những trải nghiệm thú vị và bổ ích.</p>
+<p style="line-height:18px">Hãy sớm quay lại để cùng Happy Click thăng tiến mỗi ngày nhé.</p>
+<p style="line-height:18px">Cảm ơn bạn!</p>
 
-<span style="text-align:right"> <a href="htt://dev.happyclick.vn/">Trở về trang chủ </a></span>
 
 </div>
