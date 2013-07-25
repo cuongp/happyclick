e4bc0d8578eb9c3417d42e84a770baef0a732ae9
vu nguyen <anhvunguyen2211@gmail.com>
2013-07-25 10:23:55 +0700

    update lam giau cuoc song

--- wp-content/themes/yoo_master_wp/styles/happyclick/layouts/_post-goc-kien-thuc.php
+++ wp-content/themes/yoo_master_wp/styles/happyclick/layouts/_post-goc-kien-thuc.php
@@ -1,6 +1,6 @@
 <div id="item-<?php the_ID(); ?>" class="item courses" data-permalink="<?php the_permalink(); ?>">
-	<?php if(the_post_thumbnail()) : ?>
-		<?php $width=''; $height=''; ?>
+	<?php if(has_post_thumbnail()) : ?>
+		<?php $width='180'; $height=''; ?>
 		<?php echo the_post_thumbnail(array($width,$height), ''); ?>
 	<?php endif; ?>
 		<div class="posts-content">
--- wp-content/themes/yoo_master_wp/styles/happyclick/layouts/single-goc-kien-thuc.php
+++ wp-content/themes/yoo_master_wp/styles/happyclick/layouts/single-goc-kien-thuc.php
@@ -4,7 +4,21 @@
 				<div id="sticky-download"><a href="<?php echo get_post_meta( get_the_ID(), 'URL File Upload', 'true' ); ?>" target="_blank"><h3><?php the_title(); ?></h3></a>
 				<div class="content-post-single" ><?php the_content(); ?></div></div>
 			<?php else: ?>
-				
+				<div id="item" class="first-post" data-permalink="<?php the_permalink(); ?>">
+					<h1><?php the_title(); ?></h1>
+					<div class="post-content-first">
+						<div class="img-thumb">
+							<?php if(has_post_thumbnail()): ?>
+								<?php $width=''; $height=''; ?>
+								<?php the_post_thumbnail(array($width,$height),array('class'=>'size-auto')); ?>
+							<?php endif; ?>
+						</div>
+						<?php the_content(); ?>
+					</div>
+				</div>
+				<div>
+					<?php echo $this->render('single-goc-kien-thuc-sub'); ?>
+				<div>
 			<?php endif; ?>
 		<?php endwhile; ?>
 <?php endif; ?>
