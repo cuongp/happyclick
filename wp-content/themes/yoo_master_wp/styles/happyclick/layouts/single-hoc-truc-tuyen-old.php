382b29929561924dcebfe3127b65624faf02aff7
Nguyen Tam <nguyentam.gm@gmail.com>
2013-07-29 19:13:39 +0700

    Update hoc truc tuyen

--- wp-content/themes/yoo_master_wp/styles/happyclick/layouts/single-hoc-truc-tuyen-old.php
+++ wp-content/themes/yoo_master_wp/styles/happyclick/layouts/single-hoc-truc-tuyen-old.php
@@ -20,7 +20,7 @@
 							$links = get_post_custom_values('hoc_truc_tuyen_old_tai_lieu');
 							$link = $links[0];
                             ?>
-                            <iframe width="510" height="311" src="<?php echo $youtubeVideo_link; ?>" type="application/x-shockwave-flash" frameborder="0" allowfullscreen style="float:right;" ></iframe>
+                            <iframe width="510" height="311" src="<?php echo $youtubeVideo_link; ?>?autoplay=0&amp;version=3&amp;rel=0&amp;ps=docs&amp;color=white&amp;theme=light&amp;showinfo=0&amp;hl=en_US" type="application/x-shockwave-flash" frameborder="0" allowfullscreen style="float:right;" ></iframe>
 						<p><a href="<?php echo $link; ?>"><?php _e('Download tài liệu','warp'); ?></a></p>
 					</div>
 					<h1><?php the_title(); ?></h1>
