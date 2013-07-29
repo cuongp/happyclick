db8f24c4f1d47113819a20bfbf37ae33b4c25d52
buihaian <haian@tgm.vn>
2013-07-29 12:57:21 +0700

    Fix form ho ten

--- wp-content/themes/yoo_master_wp/styles/happyclick/layouts/xem-thu.php
+++ wp-content/themes/yoo_master_wp/styles/happyclick/layouts/xem-thu.php
@@ -37,7 +37,6 @@ $db = $GLOBALS['wpdb'];
 				if($key !='action')
 					update_usermeta( $user_id, $key, $val);
 				}
-				update_usermeta($user_id,'first_name',$_POST['fullname']);
 				$db->insert($db->prefix.'m_membership_relationships',
 				array('user_id'=>$user_id
 					,'sub_id'=>0
