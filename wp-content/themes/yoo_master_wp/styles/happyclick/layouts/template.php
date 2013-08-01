8a5283b48301ba5544392114631e7e11d446cdaf
Nguyen Tam <nguyentam.gm@gmail.com>
2013-08-01 18:08:54 +0700

    Update Hoc truc tuyen

--- wp-content/themes/yoo_master_wp/styles/happyclick/layouts/template.php
+++ wp-content/themes/yoo_master_wp/styles/happyclick/layouts/template.php
@@ -20,6 +20,7 @@ global $current_user;
 <?php echo $this['template']->render('head'); ?>
 <script type="text/javascript">
 	var hcaccount = "<?php echo get_query_var('hcaccount'); ?>";
+	var backurl  = "<?php echo get_query_var('backurl'); ?>";
 	var site_url = "<?php echo get_site_url() ?>";
 </script>
 </head>
