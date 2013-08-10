3117a61a43b53ec04af64c90d3df102b8d5a4c8d
Nguyen Tam <nguyentam.gm@gmail.com>
2013-08-10 10:39:20 +0700

    Update Hoc truc tuyen

--- wp-content/themes/yoo_master_wp/styles/happyclick/layouts/_post-hoc-truc-tuyen.php
+++ wp-content/themes/yoo_master_wp/styles/happyclick/layouts/_post-hoc-truc-tuyen.php
@@ -14,9 +14,9 @@
             </p>
             <p>
                 Giảng viên: <strong>Trần Đình Dũng</strong>
-            </p>
+            </p> 
             <p class="delayed_msg">
-                Mời bạn quay lại sau ngày <em>09/08/2013</em> để đăng ký tham dự
+                Bạn đã có thể bắt đầu đăng ký khóa học trực tuyến trong tháng 8/2013
             </p>
             <p class="register-links">
                 
@@ -25,23 +25,23 @@
                 $link_class     = '';
                 $is_membership  = current_user_on_level($level_id);
                 // Tam thoi comment lai, 09/08 se mo tro lai
-//                $register_link  = get_bloginfo('url').'/thong-bao-dang-nhap/';
-//                if($is_membership){
-//                    $register_link  = 'javascript:void(0)';
-//                    $target         = '';
-//                    $link_class     = 'fancy iframe';
-//                    $webex_link     = get_post_custom_values('webex-link');
-//                    if($webex_link){
-//                        $register_link  = $webex_link[0];
-//                        $target         = 'target="_blank"';
-//                    }
-//                }
-                $register_link = 'javascript:void(0);';
+                $register_link  = get_bloginfo('url').'/thong-bao-dang-nhap/';
+                if($is_membership){
+                    $register_link  = 'javascript:void(0)';
+                    $target         = '';
+                    $link_class     = 'fancy iframe';
+                    $webex_link     = get_post_custom_values('webex-link');
+                    if($webex_link){
+                        $register_link  = $webex_link[0];
+                        $target         = 'target="_blank"';
+                    }
+                }
+//                $register_link = 'javascript:void(0);';
                 ?>
                 <?php if(!$is_membership): ?>
                     <span class="course-link-wrapper"><a class="link-orange" href="<?php echo get_bloginfo('url'); ?>/category/thanh-vien/quyen-loi-thanh-vien/"><span>Trở thành thành viên</span></a></span>
                 <?php endif; ?>
-                <span class="course-link-wrapper no-pointer"><a class="link-orange <?php echo $link_class; ?>" href="<?php echo $register_link; ?>" style="margin-right: 40px;" <?php echo $target; ?>><span>Đăng ký</span></a><span class="course-note">(Chỉ dành cho thành viên)</span></span>
+                <span class="course-link-wrapper"><a class="link-orange <?php echo $link_class; ?>" href="<?php echo $register_link; ?>" style="margin-right: 40px;" <?php echo $target; ?>><span>Đăng ký</span></a><span class="course-note">(Chỉ dành cho thành viên)</span></span>
             </p>
         </div>
     </header>
